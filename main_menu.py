6ab760065c525c846ef5bc5ac3c20a948038a33c
Jiezhoue <13809@coderacademy.edu.au>
2022-12-09 10:29:04 +1100

    create main menu for this app

--- main_menu.py
+++ main_menu.py
@@ -1,2 +1,11 @@
-print("Main menu")
-print("===========")
+def main_menu():
+    print("1. Show Menu")
+    print("2. Go to Cart")
+    print("3. Checkout")
+    print("4. Show the history")
+    print("5. exit the program")
+    opt = input("Please select the option(1-5): ")
+    return opt
+
+
+print(main_menu())
